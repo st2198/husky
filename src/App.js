695483b949e2d47a54de88da5671c073168e0a49
sterletskiy <s.terletskyi@ocado.com>
2020-12-18 15:39:00 +0200

    add husky hook

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const test = "";
+
+  // console.log(test);
   return (
     <div className="App">
       <header className="App-header">
